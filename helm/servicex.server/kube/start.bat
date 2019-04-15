592e2e002ed5d8b4ea3d8841635f924823b159e5
Ilija Vukotic <ivukotic@cern.ch>
2019-04-15 11:31:42 -0500

    kube thigs

--- helm/servicex.server/kube/start.bat
+++ helm/servicex.server/kube/start.bat
@@ -2,18 +2,18 @@ echo "Creating namespace"
 kubectl create -f namespace.yaml
 
 echo "Adding site certs"
-kubectl create secret -n gates generic cert-secret --from-file=key=secrets/certificates/gates.key.pem --from-file=cert=secrets/certificates/gates.cert.cer
-kubectl delete secret -n gates config
-kubectl create secret -n gates generic config --from-file=conf=config.json
+kubectl create secret -n servicex generic cert-secret --from-file=key=secrets/certificates/servicex.key.pem --from-file=cert=secrets/certificates/servicex.cert.cer
+kubectl delete secret -n servicex config
+kubectl create secret -n servicex generic config --from-file=conf=config.json
 
 echo "Adding globus secret"
-kubectl create secret -n gates generic globus-secret --from-file=gconf=secrets/globus-config.json
+REM kubectl create secret -n servicex generic globus-secret --from-file=gconf=secrets/globus-config.json
 
 echo "Adding MailGun secret"
-kubectl create secret -n gates generic mg-config --from-file=mgconf=secrets/mg-config.json
+REM kubectl create secret -n servicex generic mg-config --from-file=mgconf=secrets/mg-config.json
 
 echo "Create service account"
-kubectl create -f service_account.yaml
+REM kubectl create -f service_account.yaml
 
 echo "Deploying server"
 kubectl create -f frontend.yaml
