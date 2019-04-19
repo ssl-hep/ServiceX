d1743222f52e6ec8b3952c652e1c3aff70f66cbb
Ilija Vukotic <ivukotic@cern.ch>
2019-04-19 10:40:25 -0500

    cosmetics

---
+++ helm_charts/helm_charts,/helm_charts/servicex.server/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es6: true,
+    'shared-node-browser': true
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'no-console': 'off',
+    'no-plusplus': 'off',
+  },
+};
