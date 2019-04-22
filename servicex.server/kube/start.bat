7b10739e111bff80f40e163e3251e8b8e18de354
Ilija Vukotic <ivukotic@cern.ch>
2019-04-22 16:09:25 -0500

    login through globus. starting did-finder.

--- servicex.server/kube/start.bat
+++ servicex.server/kube/start.bat
@@ -2,14 +2,18 @@ echo "Creating namespace"
 kubectl create -f namespace.yaml
 
 echo "Adding site certs"
+kubectl delete secret -n servicex cert-secret
 kubectl create secret -n servicex generic cert-secret --from-file=key=secrets/servicex.key.pem --from-file=cert=secrets/servicex.cert.crt
+
+echo "Adding site conf"
 kubectl delete secret -n servicex config
 kubectl create secret -n servicex generic config --from-file=conf=config.json
 
 echo "Adding globus secret"
-REM kubectl create secret -n servicex generic globus-secret --from-file=gconf=secrets/globus-config.json
+kubectl delete secret -n servicex globus-secret
+kubectl create secret -n servicex generic globus-secret --from-file=gconf=secrets/globus-config.json
 
-echo "Adding MailGun secret"
+REM echo "Adding MailGun secret"
 REM kubectl create secret -n servicex generic mg-config --from-file=mgconf=secrets/mg-config.json
 
 echo "Create service account"
