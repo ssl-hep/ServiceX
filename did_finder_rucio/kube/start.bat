a5a8bae29dc2ef9df07c12fcd0b891850c3501d5
Ilija Vukotic <ivukotic@cern.ch>
2019-04-23 10:51:47 -0500

    pass ca_cert

--- did_finder_rucio/kube/start.bat
+++ did_finder_rucio/kube/start.bat
@@ -1,3 +1,4 @@
+kubectl apply -f secrets/rucio-secret.yaml
 kubectl delete secret -n servicex cert-secret
 kubectl create secret -n servicex generic cert-secret --from-file=userkey=secrets/xcache.key.pem --from-file=usercert=secrets/xcache.crt.pem
 
