3344156dc043e0aec34421bb706b1293f66ef8d6
Ilija Vukotic <ivukotic@cern.ch>
2019-05-07 17:17:59 -0500

    dev

--- helm_charts/servicex.server/utils/dpath.js
+++ helm_charts/servicex.server/utils/dpath.js
@@ -4,7 +4,6 @@ module.exports = function dpath(app, config) {
   const module = {};
 
   module.DApath = class DApath {
-
     constructor(id = null) {
       this.es = new elasticsearch.Client({ host: config.ES_HOST, log: 'error' });
       this.created_at = new Date().getTime();
@@ -41,7 +40,35 @@ module.exports = function dpath(app, config) {
     }
 
     async get(id) {
-      console.log('getting all path info for id:', id);
+      console.log('getting path info for id:', id);
+      try {
+        const response = await this.es.search({
+          index: 'servicex_paths',
+          type: 'docs',
+          body: {
+            query: {
+              match: { _id: id },
+            },
+          },
+        });
+        // console.log(response);
+        if (response.hits.total === 0) {
+          console.log('data access path not found.');
+          return false;
+        }
+        console.log('data request found.');
+        const obj = response.hits.hits[0]._source;
+        return obj;
+      } catch (err) {
+        console.error(err);
+      }
+      console.log('Done.');
+      return false;
+    }
+
+    // TODO - not finished.
+    async getAll(rid) {
+      console.log('getting ids of all paths of rid:', rid);
       try {
         const response = await this.es.search({
           index: 'servicex_paths',
@@ -50,8 +77,57 @@ module.exports = function dpath(app, config) {
             query: {
               bool: {
                 must: [
-                  { match: { _id: id } },
+                  { match: { req_id: rid } },
                 ],
+              },
+            },
+          },
+        });
+        // console.log(response);
+        if (response.hits.total === 0) {
+          console.log('no data access paths.');
+          return [];
+        }
+
+        console.log('data access paths found.');
+        // const obj = response.hits.hits[0]._source;
+        // // console.log(obj);
+        // this.id = response.hits.hits[0]._id;
+        // this.name = obj.name;
+        // this.description = obj.description;
+        // this.dataset = obj.dataset;
+        // this.columns = obj.columns;
+        // this.events = obj.events;
+        // this.status = obj.status;
+        // this.info = obj.info;
+        // this.dataset_size = obj.dataset_size;
+        // this.dataset_files = obj.dataset_files;
+        // this.dataset_events = obj.dataset_events;
+        // this.events_served = obj.events_served;
+        // this.events_processed = obj.events_processed;
+        // this.created_at = obj.created_at;
+        // return true;
+      } catch (err) {
+        console.error(err);
+      }
+      console.log('Done.');
+      return false;
+    }
+
+    async getLastUsed(rid) {
+      console.log('getting path info for last used path of rid:', rid);
+      try {
+        const response = await this.es.search({
+          index: 'servicex_paths',
+          type: 'docs',
+          body: {
+            size: 1,
+            query: {
+              match: { req_id: rid },
+            },
+            sort: {
+              last_accessed_at: {
+                order: 'desc',
               },
             },
           },
@@ -93,6 +169,15 @@ module.exports = function dpath(app, config) {
     console.log('getting a path with id: ', id);
     const DApath = new module.DApath();
     const dap = await DApath.get(id);
+    console.log('sending back:', dap);
+    res.status(200).json(dap);
+  });
+
+  app.get('/dpath/last_used/:rid', async (req, res) => {
+    const { rid } = req.params;
+    console.log('getting a last used path with rid: ', rid);
+    const DApath = new module.DApath();
+    const dap = await DApath.getLastUsed(rid);
     console.log('sending back:', dap);
     res.status(200).json(dap);
   });
