0448b851d64358a55aa9e7f27176f5e372fe14c8
Ilija Vukotic <ivukotic@cern.ch>
2019-05-07 13:22:25 -0500

    endpoint to return request with a certain status

--- servicex.server/controllers/Default.js
+++ servicex.server/controllers/Default.js
@@ -1,9 +1,7 @@
-'use strict';
-
 var utils = require('../utils/writer.js');
 var Default = require('../service/DefaultService');
 
-module.exports.dataPOST = function dataPOST (req, res, next) {
+module.exports.dataPOST = function dataPOST(req, res, next) {
   var body = req.swagger.params['body'].value;
   Default.dataPOST(body)
     .then(function (response) {
@@ -14,7 +12,7 @@ module.exports.dataPOST = function dataPOST (req, res, next) {
     });
 };
 
-module.exports.dataRequest_idGET = function dataRequest_idGET (req, res, next) {
+module.exports.dataRequest_idGET = function dataRequest_idGET(req, res, next) {
   var request_id = req.swagger.params['request_id'].value;
   Default.dataRequest_idGET(request_id)
     .then(function (response) {
@@ -25,7 +23,7 @@ module.exports.dataRequest_idGET = function dataRequest_idGET (req, res, next) {
     });
 };
 
-module.exports.healthzGET = function healthzGET (req, res, next) {
+module.exports.healthzGET = function healthzGET(req, res, next) {
   Default.healthzGET()
     .then(function (response) {
       utils.writeJson(res, response);
