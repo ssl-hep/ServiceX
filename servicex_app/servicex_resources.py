836b68df380bcc15c8dc62baace64f06b7211981
Ben Galewsky <bengal1@illinois.ed>
2019-08-16 17:39:48 -0500

    Refactor interactions with services

--- servicex_app/servicex_resources.py
+++ servicex_app/servicex_resources.py
@@ -28,10 +28,12 @@
 import json
 
 import pika
+from flask import request
 from flask_restful import Resource, reqparse
 import uuid
 from models import TransformRequest
 from run import app
+from transformer_manager import launch_transformer_jobs
 
 rabbitmq = pika.BlockingConnection(
     pika.ConnectionParameters(
@@ -48,27 +50,43 @@ parser.add_argument('did', help='This field cannot be blank',
                     required=True)
 parser.add_argument('columns', help='This field cannot be blank',
                     required=True)
+parser.add_argument('image', required=False)
+parser.add_argument('chunk-size', required=False, type=int)
+parser.add_argument('workers', required=False, type=int)
+parser.add_argument('messaging-backend', required=False)
+parser.add_argument('kafka-broker', required=False)
 
 
 class SubmitTransformationRequest(Resource):
     def post(self):
         request = parser.parse_args()
-        request_id = uuid.uuid4()
+        request_id = str(uuid.uuid4())
 
         request_rec = TransformRequest(
             did=request['did'],
             columns=request['columns'],
-            request_id=str(request_id)
+            request_id=str(request_id),
+            image=request['image'],
+            chunk_size=request['chunk-size'],
+            messaging_backend=request['messaging-backend'],
+            kafka_broker=request['kafka-broker'],
+            workers=request['workers']
         )
 
         did_request = {
             "request_id": request_rec.request_id,
             "did": request_rec.did,
-            "columns": request_rec.columns,
-            "status-endpoint": "http://host.docker.internal:5000/servicex/transformation/"+request_rec.request_id+"/status"
+            "service-endpoint":
+                "http://host.docker.internal:5000/servicex/transformation/" +
+                request_rec.request_id
         }
 
         try:
+            channel.queue_declare(request_id)
+            channel.queue_bind(exchange="transformation_requests",
+                               queue=request_id,
+                               routing_key=request_id)
+
             channel.basic_publish(exchange='',
                                   routing_key='did_requests',
                                   body=json.dumps(did_request))
@@ -100,6 +118,79 @@ class TransformationStatus(Resource):
 class QueryTransformationRequest(Resource):
     def get(self, request_id=None):
         if request_id:
-            return TransformRequest.return_request(request_id)
+            return TransformRequest.to_json(
+                TransformRequest.return_request(request_id)
+            )
         else:
             return TransformRequest.return_all()
+
+
+class AddFileToDataset(Resource):
+    def put(self, request_id):
+        add_file_request = request.get_json()
+        submitted_request = TransformRequest.return_request(request_id)
+
+        transform_request = {
+            'request-id': submitted_request.request_id,
+            'columns': submitted_request.columns,
+            'file_path': add_file_request['file_path'],
+            "status-endpoint":
+                "http://host.docker.internal:5000/servicex/transformation/" +
+                submitted_request.request_id + "/status"
+        }
+
+        try:
+            channel.basic_publish(exchange='transformation_requests',
+                                  routing_key=request_id,
+                                  body=json.dumps(transform_request))
+
+            return {
+                "request-id": str(request_id),
+                "file-id": 42
+            }
+
+        except Exception as eek:
+            print(eek)
+            return {'message': 'Something went wrong'}, 500
+
+
+class PreflightCheck(Resource):
+    def post(self, request_id):
+        body = request.get_json()
+        submitted_request = TransformRequest.return_request(request_id)
+
+        preflight_request = {
+            'request-id': submitted_request.request_id,
+            'columns': submitted_request.columns,
+            'file-path': body['file_path'],
+            "service-endpoint":
+                "http://host.docker.internal:5000/servicex/transformation/" +
+                request_id
+        }
+
+        try:
+            channel.basic_publish(exchange='',
+                                  routing_key='validation_requests',
+                                  body=json.dumps(preflight_request))
+
+            return {
+                "request-id": str(request_id),
+                "file-id": 42
+            }
+
+        except Exception as eek:
+            print(eek)
+            return {'message': 'Something went wrong'}, 500
+
+
+class FilesetComplete(Resource):
+    def put(self, request_id):
+        print("Complete "+request_id)
+
+
+class TransformStart(Resource):
+    def post(self, request_id):
+        info = request.get_json()
+        submitted_request = TransformRequest.return_request(request_id)
+        if app.config['TRANSFORMER_MANAGER_ENABLED']:
+            launch_transformer_jobs(request_id, submitted_request.workers)
