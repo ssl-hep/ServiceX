abe56a879afd0f2bd1cf3426c2651760908378c7
Ilija Vukotic <ivukotic@cern.ch>
2019-09-11 12:55:10 -0500

    changed config for frontend

--- servicex.frontend/kube/run.bat
+++ servicex.frontend/kube/run.bat
@@ -1,6 +1,6 @@
 
 REM __create secret with configuration__  
-kubectl create secret -n servicex generic config --from-file=conf=../config/config.json
+kubectl create secret -n servicex generic config-frontend --from-file=conf=../config/config.json
 
 REM __create secret for Elasticsearch access__  
 kubectl create secret -n servicex generic es-secret --from-file=es_conf=secrets/elasticsearch/elasticsearch.json
