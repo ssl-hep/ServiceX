3dbbdf749d16a75799f940183bb4f754a7e142f4
Ilija Vukotic <ivukotic@cern.ch>
2019-09-11 13:30:07 -0500

    change config

--- servicex.server/kube/run.bat
+++ servicex.server/kube/run.bat
@@ -1,6 +1,6 @@
 
 REM __create secret with configuration__  
-kubectl create secret -n servicex generic config --from-file=conf=../config/config.json
+kubectl create secret -n servicex generic config-web --from-file=conf=../config/config.json
 
 REM __create secret for globus__  
 kubectl create secret -n servicex generic globus-secret --from-file=gconf=../../kube/secrets/globus-conf/globus-config.json
