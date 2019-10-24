f8f5db5240fc3e12a6b31da42b7d9037b0bd35cd
Ben Galewsky <bengal1@illinois.ed>
2019-10-24 14:29:58 -0500

    Fix flake8 problem

--- helm_charts/helm_charts,/servicex_app/servicex/resources/transform_start.py
+++ helm_charts/helm_charts,/servicex_app/servicex/resources/transform_start.py
@@ -25,7 +25,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-from flask import current_app, request
+from flask import current_app
 
 from servicex.models import TransformRequest
 from servicex.resources.servicex_resource import ServiceXResource
