0a25cd0a7613ab86e7e7dd3358812885e712ff78
Ben Galewsky <ben@peartreestudio.net>
2019-12-20 15:40:21 -0800

    Add template transformer script

---
+++ xaod_cpp_transformer/xAOD_CPP_Transformer.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2019, IRIS-HEP
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import json
+
+from servicex.transformer.transformer_argument_parser import TransformerArgumentParser
+from servicex.transformer.kafka_messaging import KafkaMessaging
+from servicex.transformer.object_store_manager import ObjectStoreManager
+from servicex.transformer.rabbit_mq_manager import RabbitMQManager
+
+
+# How many bytes does an average awkward array cell take up. This is just
+# a rule of thumb to calculate chunksize
+avg_cell_size = 42
+
+
+# noinspection PyUnusedLocal
+def callback(channel, method, properties, body):
+    transform_request = json.loads(body)
+    _request_id = transform_request['request-id']
+    _tree_name = transform_request['tree-name']
+    _file_path = transform_request['file-path']
+    _file_id = transform_request['file-id']
+    _server_endpoint = transform_request['service-endpoint']
+
+    print(_file_path)
+    try:
+        # Do the transform
+        pass
+
+    except Exception as error:
+        transform_request['error'] = str(error)
+        channel.basic_publish(exchange='transformation_failures',
+                              routing_key=_request_id + '_errors',
+                              body=json.dumps(transform_request))
+        # arrow_writer.put_file_complete(file_path=_file_path, file_id=_file_id,
+        #                                status='failure', num_messages=0, total_time=0,
+        #                                total_events=0, total_bytes=0)
+    finally:
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def transform_single_file(file_path, tree, attr_list, chunk_size):
+    print("Transforming a single path: " + str(args.path))
+
+
+if __name__ == "__main__":
+    parser = TransformerArgumentParser(description="xAOD CPP Transformer")
+    args = parser.parse_args()
+
+    kafka_brokers = TransformerArgumentParser.extract_kafka_brokers(args.brokerlist)
+
+    if args.result_destination == 'kafka':
+        messaging = KafkaMessaging(kafka_brokers, args.max_message_size)
+        object_store = None
+    elif args.result_destination == 'object-store':
+        messaging = None
+        object_store = ObjectStoreManager()
+
+    _attr_list = TransformerArgumentParser.extract_attr_list(args.attr_names)
+
+    chunk_size = args.chunks
+
+    if args.request_id and not args.path:
+        rabbitmq = RabbitMQManager(args.rabbit_uri, args.request_id, callback)
+
+    if args.path:
+        transform_single_file(args.path, args.tree, _attr_list, chunk_size)
