cc8844450f89851f3f9e96f34a12d2d2b253314f
Ben Galewsky <ben@peartreestudio.net>
2020-01-28 12:56:50 -0600

    Add app config file env var to boot.sh

--- code_generator_funcadl_xAOD/boot.sh
+++ code_generator_funcadl_xAOD/boot.sh
@@ -3,6 +3,7 @@
 # Running the web server or a utility?
 action=${1:-web_service}
 if [ "$action" = "web_service" ] ; then
+    export APP_CONFIG_FILE='/home/servicex/app.conf'
     mkdir instance
     exec gunicorn -b :5000 --workers=2 --threads=1 --access-logfile - --error-logfile - "servicex.code_generator_service:create_app()"
 elif [ "$action" = "translate" ]; then
