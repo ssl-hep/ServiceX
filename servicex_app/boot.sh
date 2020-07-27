26e6befccb06a9ffb49c38349db52913cbc04b2b
Ben Galewsky <ben@peartreestudio.net>
2020-07-27 11:30:10 -0500

    Enable alembic database migration
    Update README

--- servicex_app/boot.sh
+++ servicex_app/boot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir instance
+FLASK_APP=servicex/app.py flask db upgrade
 #flask db upgrade
 #flask translate compile
 exec gunicorn -b :5000 --workers=5 --threads=1 --timeout 120 --access-logfile - --error-logfile - "servicex:create_app()"
