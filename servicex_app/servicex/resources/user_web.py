ce8053a11349b6b4ffb041ff08bb31dc08ac8285
Andrew Eckart <andrew.g.eckart@gmail.com>
2020-08-04 11:34:21 -0500

    Generate refresh tokens instead of API keys

--- servicex_app/servicex/resources/user_web.py
+++
@@ -1,5 +0,0 @@
-from flask import render_template
-
-
-def index():
-    return render_template('index.html', title='Home')
--- servicex_app/servicex/web/__init__.py
+++ servicex_app/servicex/web/__init__.py
@@ -2,7 +2,7 @@ from .home import home
 from .sign_in import sign_in
 from .sign_out import sign_out
 from .auth_callback import auth_callback
-from .api_key import api_key
+from .api_token import api_token
 from .create_profile import create_profile
 from .view_profile import view_profile
 from .edit_profile import edit_profile
@@ -13,7 +13,7 @@ __all__ = [
     sign_in,
     sign_out,
     auth_callback,
-    api_key,
+    api_token,
     create_profile,
     view_profile,
     edit_profile,
