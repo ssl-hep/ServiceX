3a38f58a488d7f08a1a4fb85f2215c0d5df114c8
Andrew Eckart <andrew.g.eckart@gmail.com>
2020-08-18 14:44:22 -0500

    Add MailgunAdaptor and tests

---
+++ servicex_app/tests/test_mailgun_adaptor.py
@@ -0,0 +1,40 @@
+from flask import render_template
+
+from servicex.mailgun_adaptor import MailgunAdaptor
+from tests.resource_test_base import ResourceTestBase
+
+
+class TestMailgunAdaptor(ResourceTestBase):
+    def test_noop(self, mocker, mock_rabbit_adaptor):
+        import requests
+        mock_post = mocker.patch.object(requests, "post")
+        client = self._test_client(rabbit_adaptor=mock_rabbit_adaptor)
+        with client.application.app_context():
+            mailgun = MailgunAdaptor()
+            mailgun.send('janedoe@example.com', 'welcome.html')
+            mock_post.assert_not_called()
+
+    def test_init(self, mock_rabbit_adaptor):
+        config = {'MAILGUN_API_KEY': 'key123', 'MAILGUN_DOMAIN': 'example.com'}
+        client = self._test_client(rabbit_adaptor=mock_rabbit_adaptor,
+                                   extra_config=config)
+        with client.application.app_context():
+            mailgun = MailgunAdaptor()
+            assert mailgun.api_key == config['MAILGUN_API_KEY']
+            assert mailgun.domain == config['MAILGUN_DOMAIN']
+
+    def test_send(self, mocker, mock_rabbit_adaptor):
+        import requests
+        mock_post = mocker.patch.object(requests, "post")
+        config = {'MAILGUN_API_KEY': 'key123', 'MAILGUN_DOMAIN': 'example.com'}
+        client = self._test_client(rabbit_adaptor=mock_rabbit_adaptor,
+                                   extra_config=config)
+        with client.application.app_context():
+            mailgun = MailgunAdaptor()
+            email, template_name = 'janedoe@example.com', 'welcome.html'
+            mailgun.send(email, template_name)
+            mock_post.assert_called_once()
+            args, kwargs = mock_post.call_args
+            endpoint_arg, data_arg = args
+            assert data_arg['to'] == [email]
+            assert data_arg['html'] == render_template(f"emails/{template_name}")
