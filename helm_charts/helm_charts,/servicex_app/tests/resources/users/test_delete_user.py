de4e8c0b5b9ea826c0d31ea9c752cc4218bf1c1c
Andrew Eckart <andrew.g.eckart@gmail.com>
2020-08-24 14:27:46 -0500

    Add test case for delete user 404 case

--- helm_charts/helm_charts,/servicex_app/tests/resources/users/test_delete_user.py
+++ helm_charts/helm_charts,/servicex_app/tests/resources/users/test_delete_user.py
@@ -12,3 +12,10 @@ class TestDeleteUser(WebTestBase):
         assert resp.status_code == 200
         assert resp.json == resp_json
         assert user.delete_from_db.called_once()
+
+    def test_delete_user_missing(self, client):
+        fake_user_id = 12345
+        resp_json = {'message': f'user {fake_user_id} not found'}
+        resp: Response = client.delete(f'users/{fake_user_id}')
+        assert resp.status_code == 404
+        assert resp.json == resp_json
