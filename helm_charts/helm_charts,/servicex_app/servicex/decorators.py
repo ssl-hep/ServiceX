e071b10fa82c91a65eb49ab35056e14e0e99cede
Andrew Eckart <andrew.g.eckart@gmail.com>
2020-09-20 10:24:34 -0500

    Add integration tests for auth_required decorator

--- helm_charts/helm_charts,/servicex_app/servicex/decorators.py
+++ helm_charts/helm_charts,/servicex_app/servicex/decorators.py
@@ -33,11 +33,7 @@ def auth_required(fn: Callable[..., Response]) -> Callable[..., Response]:
     Pending or deleted users will receive a 401: Unauthorized response.
     """
 
-    if not current_app.config.get('ENABLE_AUTH'):
-        return fn
-
-    @wraps(fn)
-    def decorated_function(*args, **kwargs) -> Response:
+    def inner(*args, **kwargs) -> Response:
         user = UserModel.find_by_sub(get_jwt_identity())
         if not user:
             msg = 'Not Authorized: No user found matching this API token. ' \
@@ -51,21 +47,31 @@ def auth_required(fn: Callable[..., Response]) -> Callable[..., Response]:
             return make_response({'message': msg}, 401)
         return fn(*args, **kwargs)
 
-    return jwt_required(decorated_function)
+    @wraps(fn)
+    def decorated(*args, **kwargs) -> Callable[..., Response]:
+        if not current_app.config.get('ENABLE_AUTH'):
+            return fn(*args, **kwargs)
+        # Wrap with jwt_required as well if auth is enabled
+        return jwt_required(inner)(*args, **kwargs)
+
+    return decorated
 
 
 def admin_required(fn: Callable[..., Response]) -> Callable[..., Response]:
     """Mark an API resource as requiring administrator role."""
 
-    if not current_app.config.get('ENABLE_AUTH'):
-        return fn
-
-    @wraps(fn)
-    def decorated_function(*args, **kwargs) -> Response:
+    def inner(*args, **kwargs) -> Response:
         user = UserModel.find_by_sub(get_jwt_identity())
         msg = 'Not Authorized: This resource is restricted to administrators.'
         if not (user and user.admin):
             return make_response({'message': msg}, 401)
         return fn(*args, **kwargs)
 
-    return jwt_required(decorated_function)
+    @wraps(fn)
+    def decorated(*args, **kwargs) -> Callable[..., Response]:
+        if not current_app.config.get('ENABLE_AUTH'):
+            return fn(*args, **kwargs)
+        # Wrap with jwt_required as well if auth is enabled
+        return jwt_required(inner)(*args, **kwargs)
+
+    return decorated
