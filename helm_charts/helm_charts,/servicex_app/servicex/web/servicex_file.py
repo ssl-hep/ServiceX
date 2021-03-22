8260b59b3a70fc2437e10538ca02f540c6cf2c6b
Suchandra Thapa <suchandra@gmail.com>
2021-03-21 20:15:32 -1000

    move code to separate function and add unit test

--- helm_charts/helm_charts,/servicex_app/servicex/web/servicex_file.py
+++ helm_charts/helm_charts,/servicex_app/servicex/web/servicex_file.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 from textwrap import dedent
+from urllib.parse import urlparse
 
 from flask import current_app, flash, redirect, request, session, url_for, \
     send_file
@@ -22,11 +23,30 @@ def servicex_file():
     backend_type = matches[0]
     sub = session.get('sub')
     user = UserModel.find_by_sub(sub)
+
+    endpoint_url = correct_url(request.url_root)
     body = f"""\
     api_endpoints:
-      - endpoint: {request.url_root}
+      - endpoint: {endpoint_url}
         token: {user.refresh_token}
         type: {backend_type}
     """
     return send_file(BytesIO(dedent(body).encode()), mimetype="text/plain",
                      as_attachment=True, attachment_filename="servicex.yaml")
+
+
+def correct_url(url: str) -> str:
+    """
+    Update url string to remove http reference in uri to https reference
+    unless the url refers to localhost
+    see https://github.com/ssl-hep/ServiceX/issues/266
+
+    :param url: string with complete url
+    :return: string with http url changed to https except
+    """
+
+    parsed_url = urlparse(url)
+    if parsed_url.scheme == "http" and "localhost" not in parsed_url.netloc:
+        return parsed_url._replace(scheme="https").geturl()
+    else:
+        return url
--- helm_charts/helm_charts,/servicex_app/tests/web/test_servicex_file.py
+++ helm_charts/helm_charts,/servicex_app/tests/web/test_servicex_file.py
@@ -4,6 +4,7 @@ from flask import Response, url_for, get_flashed_messages
 
 from .web_test_base import WebTestBase
 
+from servicex.web.servicex_file import correct_url
 
 class TestServiceXFile(WebTestBase):
     def test_servicex_file(self, client, user):
@@ -36,3 +37,27 @@ class TestServiceXFile(WebTestBase):
         flashed_messages = get_flashed_messages()
         assert flashed_messages
         assert "Unable to infer filetype" in flashed_messages[0]
+
+    def test_correct_url(self, client):
+        """
+        Test that http endpoints are replaced with https addresses with the
+        exception of http endpoints that point to localhost
+
+        see:
+        https://github.com/ssl-hep/ServiceX/issues/266
+        """
+        test_url = "http://localhost/foo/test"
+        result = correct_url(test_url)
+        assert result == test_url
+
+        test_url = "https://localhost/foo/test"
+        result = correct_url(test_url)
+        assert result == test_url
+
+        test_url = "http://test.com/foo/test"
+        result = correct_url(test_url)
+        assert result == test_url.replace("http", "https")
+
+        test_url = "https://test.com:8080/foo/test"
+        result = correct_url(test_url)
+        assert result == test_url
