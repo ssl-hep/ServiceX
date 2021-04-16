b77698374735ccb07d6c09757f16a6bea10c6392
Andrew Eckart <andrew.g.eckart@gmail.com>
2021-04-16 11:33:37 -0500

    Add DeploymentStatus endpoint and tests

---
+++ helm_charts/helm_charts,/servicex_app/servicex/resources/deployment_status.py
@@ -0,0 +1,18 @@
+from flask import jsonify
+
+from servicex.decorators import auth_required
+from servicex.resources.servicex_resource import ServiceXResource
+from servicex.resources.transform_start import TransformStart
+from servicex.transformer_manager import TransformerManager
+
+
+class DeploymentStatus(ServiceXResource):
+    @auth_required
+    def get(self, request_id):
+        # todo - improve dependency injection
+        manager: TransformerManager = TransformStart.transformer_manager
+        status = manager.get_deployment_status(request_id)
+        if status is None:
+            msg = f"Deployment not found for request with id: '{request_id}'"
+            return {'message': msg}, 404
+        return jsonify(status.to_dict())
