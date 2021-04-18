24014eecf41354d7649211b0f778a4fbdf5c48ad
Gordon Watts <gwatts@uw.edu>
2021-04-18 01:01:33 -0700

    Initial commit

---
+++ did_finder_cernopendata/README.md
@@ -0,0 +1,2 @@
+# ServiceX_DID_finder_CERNOpenData
+ Access datasets for ServiceX from CERN Open Data Portal
---
+++ did_finder_cernopendata/tests/test_servicex_did_finder_cernopendata.py
@@ -0,0 +1,5 @@
+from servicex_did_finder_cernopendata import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
