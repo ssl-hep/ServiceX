a50b383a27bda8d3a9f927e77d047c4222c16424
Andrew Eckart <andrew.g.eckart@gmail.com>
2021-04-29 12:50:45 -0500

    Use environment variable for app config file path in boot script

--- servicex_app/boot.sh
+++ servicex_app/boot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir instance
 # SQLite doesn't handle migrations, so rely on SQLAlchmy table creation
-if grep "sqlite://" /opt/servicex/app.conf; then
+if grep "sqlite://" $APP_CONFIG_FILE; then
   echo "SQLLite DB, so skipping db migrations";
 else
   FLASK_APP=servicex/app.py flask db upgrade;
