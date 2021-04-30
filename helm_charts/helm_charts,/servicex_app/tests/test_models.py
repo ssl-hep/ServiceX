e803b475d06ec573b2f44992b11f64e1e877d164
Andrew Eckart <andrew.g.eckart@gmail.com>
2021-04-30 11:55:06 -0500

    Add tests for submitter_name

--- helm_charts/helm_charts,/servicex_app/tests/test_models.py
+++ helm_charts/helm_charts,/servicex_app/tests/test_models.py
@@ -1,7 +1,29 @@
-from servicex.models import TransformRequest
+from servicex.models import UserModel, TransformRequest
 
 
 class TestTransformRequest:
+
+    def test_submitter_name(self):
+        user = UserModel()
+        user.id = 1234
+        user.name = "Leonardo"
+        request = TransformRequest()
+        request.submitted_by = user.id
+        request.user = user
+        assert request.submitter_name == user.name
+
+    def test_submitted_name_deleted(self):
+        user = UserModel()
+        user.id = 1234
+        user.name = "Leonardo"
+        request = TransformRequest()
+        request.submitted_by = user.id
+        request.user = None
+        assert request.submitter_name == "[deleted]"
+
+    def test_submitted_name_none(self):
+        request = TransformRequest()
+        assert request.submitter_name is None
 
     def test_files_remaining(self, mocker):
         request = TransformRequest()
