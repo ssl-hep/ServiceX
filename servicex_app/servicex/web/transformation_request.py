3d7a3caf30feae1e25a55cb51e1d249506216df7
Andrew Eckart <andrew.g.eckart@gmail.com>
2021-05-01 18:58:51 -0500

    Cleanup, protect route

--- servicex_app/servicex/web/transformation_request.py
+++ servicex_app/servicex/web/transformation_request.py
@@ -3,16 +3,16 @@ from datetime import timedelta
 from flask import render_template, abort
 import humanize
 
+from servicex.decorators import oauth_required
 from servicex.models import TransformRequest, db
 
 
+@oauth_required
 def transformation_request(id_: str):
-    print(id_)
     if id_.isnumeric():
         req = TransformRequest.query.get(id_)
     else:
         req = db.session.query(TransformRequest).filter_by(request_id=id_).one()
-    print(req)
     if not req:
         abort(404)
     return render_template(
