378b78b8b89ad3587163e4546012e7caefbb9980
Andrew Eckart <andrew.g.eckart@gmail.com>
2021-05-08 11:25:56 -0500

    C0411 (wrong-import-order)

--- helm_charts/helm_charts,/servicex_app/servicex/resources/file_transform_status.py
+++ helm_charts/helm_charts,/servicex_app/servicex/resources/file_transform_status.py
@@ -25,11 +25,12 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import datetime
+
 from flask_restful import reqparse
 from servicex.models import db
 from servicex.resources.servicex_resource import ServiceXResource
 from servicex.models import FileStatus, max_string_size
-import datetime
 
 
 class FileTransformationStatus(ServiceXResource):
