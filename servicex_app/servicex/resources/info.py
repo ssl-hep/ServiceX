2f09bb5ae9f84cadfa6017828d3300b9b3e0787b
Andrew Eckart <andrew.g.eckart@gmail.com>
2021-05-08 11:25:56 -0500

    C0411 (wrong-import-order)

--- servicex_app/servicex/resources/info.py
+++ servicex_app/servicex/resources/info.py
@@ -25,8 +25,9 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-from servicex.resources.servicex_resource import ServiceXResource
 from flask import current_app
+
+from servicex.resources.servicex_resource import ServiceXResource
 
 
 class Info(ServiceXResource):
