4cb0dbcb396a70ba95105c4e3316d2f059535bfc
Gordon Watts <gwatts@uw.edu>
2021-07-17 13:20:22 -0400

    OpenData Test Fixes (#2)
    Make the error message for a blank line more clear by adding a quote
    Properly capture output and dump it when process exits
    Only dump malformed url error message if everything else looks good
    Added CI to track changes and regressions

--- did_finder_cernopendata/src/servicex_did_finder_cernopendata/did_finder.py
+++ did_finder_cernopendata/src/servicex_did_finder_cernopendata/did_finder.py
@@ -7,7 +7,10 @@ from servicex_did_finder_lib import start_did_finder
 __log = logging.getLogger(__name__)
 
 
-async def find_files(did_name: str, info: Dict[str, Any]) -> AsyncGenerator[Dict[str, Any], None]:
+async def find_files(did_name: str,
+                     info: Dict[str, Any],
+                     command: str = 'cernopendata-client'
+                     ) -> AsyncGenerator[Dict[str, Any], None]:
     '''For each incoming did name, generate a list of files that ServiceX can
     process
 
@@ -19,6 +22,9 @@ async def find_files(did_name: str, info: Dict[str, Any]) -> AsyncGenerator[Dict
 
     Args:
         did_name (str): Dataset name
+        into (Dict[str, Any]): Information bag, mainly has the `request-id` which is
+                               used to track error mesages accross logs.
+        command (str): Command to execute to get the did information. Used only for testing.
 
     Returns:
         AsyncGenerator[Dict[str, any], None]: yield each file
@@ -28,23 +34,38 @@ async def find_files(did_name: str, info: Dict[str, Any]) -> AsyncGenerator[Dict
     if not did_name.isnumeric():
         raise Exception('CERNOpenData can only work with dataset numbers as names (e.g. 1507)')
 
-    cmd = f'cernopendata-client get-file-locations --protocol xrootd --recid {did_name}'.split(' ')
-    print(cmd)
+    cmd = f'{command} get-file-locations --protocol xrootd --recid {did_name}'.split(' ')
 
     with Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1,
                universal_newlines=1) as p:  # type: ignore
 
+        assert p.stdout is not None
+        all_lines = []
+        non_root_uri = False
         for line in p.stdout:
             assert isinstance(line, str)
+            all_lines.append(line)
             uri = line.strip()
             if not uri.startswith('root://'):
-                raise Exception(f'CMSOpenData: Opendata record returned a non-xrootd url: {uri}')
-            yield {
-                'file_path': uri,
-                'adler32': 0,  # No clue
-                'file_size': 0,  # Size in bytes if known
-                'file_events': 0,  # Number of events if known
-            }
+                non_root_uri = True
+            else:
+                yield {
+                    'file_path': uri,
+                    'adler32': 0,  # No clue
+                    'file_size': 0,  # Size in bytes if known
+                    'file_events': 0,  # Number of events if known
+                }
+
+        # Next, sort out the errors (if there are any)
+        p.wait()
+        if p.returncode != 0:
+            raise Exception(f'CERN Open Data Lookup failed with error code {p.returncode}. '
+                            'All returned output:'
+                            '\n\t' + '\n\t'.join(all_lines))
+
+        if non_root_uri:
+            raise Exception('CMSOpenData: Opendata record returned a non-xrootd url'
+                            '\n\t' + '\n\t'.join(all_lines))
 
 
 def run_open_data():
