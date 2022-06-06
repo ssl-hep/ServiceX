4d4c91508685155e038847286e19a2fc5b66a393
Ilija Vukotic <ivukotic@cern.ch>
2022-06-06 09:54:36 -0500

    fixing logging

--- helm_charts/helm_charts,/servicex_app/servicex/resources/internal/add_file_to_dataset.py
+++ helm_charts/helm_charts,/servicex_app/servicex/resources/internal/add_file_to_dataset.py
@@ -42,7 +42,8 @@ class AddFileToDataset(ServiceXResource):
             from servicex.models import db
             add_file_request = request.get_json()
             submitted_request = TransformRequest.lookup(request_id)
-            self.logger.info(f"Submitted request: {submitted_request} for {request_id}")
+            current_app.logger.info(f"Submitted request: {submitted_request}",
+                                    extra={'requestId': request_id})
 
             # check if the request is bulk or single file
             if type(add_file_request) is dict:
@@ -58,7 +59,8 @@ class AddFileToDataset(ServiceXResource):
                 self.lookup_result_processor.add_file_to_dataset(submitted_request, db_record)
 
                 db.session.commit()
-                self.logger.info(f"Got file-id: {db_record.id} for request-id:{str(request_id)}")
+                current_app.logger.info(f"Got file-id: {db_record.id}",
+                                        extra={'requestId': request_id})
 
             return {
                 "request-id": str(request_id)
