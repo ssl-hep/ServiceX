f3c3b492078d7e8f95719892c253e7bbdd3bb0c0
Prajwal  Kiran Kumar <prajwalkirankumar@Prajwals-MacBook-Pro.local>
2022-10-19 22:32:46 -0500

    Fixes for helm release ci

--- .github/workflows/update_versions.py
+++ .github/workflows/update_versions.py
@@ -33,7 +33,7 @@ def update_version_in_servicex_chart_yaml(version):
     servicex_chart_yaml_file = "servicex/Chart.yaml"
     with open(servicex_chart_yaml_file, 'r') as stream:
         try:
-            servicex_chart_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader)
+            servicex_chart_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader,preserve_quotes=True)
         except ruamel.yaml.YAMLError as exc:
             print("ERR: loading Yaml file")
             raise Exception(exc)
@@ -49,7 +49,7 @@ def update_version_in_servicex_values_yaml(version):
     servicex_values_yaml_file = "servicex/values.yaml"
     with open(servicex_values_yaml_file, 'r') as stream:
         try:
-            servicex_values_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader)
+            servicex_values_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader,preserve_quotes=True)
         except ruamel.yaml.YAMLError as exc:
             print("ERR: loading Yaml file")
             raise Exception(exc)
@@ -69,7 +69,7 @@ def update_version_in_flux_river_uproot_values_yaml(version):
     flux_river_uproot_values_file = "../flux_river_configs/servicex-int-uproot/values.yaml"
     with open(flux_river_uproot_values_file, 'r') as stream:
         try:
-            flux_river_uproot_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader)
+            flux_river_uproot_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader,preserve_quotes=True)
         except ruamel.yaml.YAMLError as exc:
             print(exc)
 
@@ -83,7 +83,7 @@ def update_version_in_flux_river_xaod_values_yaml(version):
 
     with open(flux_river_xaod_values_file, 'r') as stream:
         try:
-            flux_river_xaod_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader)
+            flux_river_xaod_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader,preserve_quotes=True)
         except ruamel.yaml.YAMLError as exc:
             print(exc)
 
@@ -99,9 +99,9 @@ if __name__ == '__main__':
         raise Exception("ERR: Please enter version to update.")
 
     version = sys.argv[1]
-    update_version_in_servicex_chart_yaml(version)
-    update_version_in_servicex_values_yaml(version)
-    update_version_in_flux_river_uproot_values_yaml(version)
+    # update_version_in_servicex_chart_yaml(version)
+    # update_version_in_servicex_values_yaml(version)
+    # update_version_in_flux_river_uproot_values_yaml(version)
     update_version_in_flux_river_xaod_values_yaml(version)
 
 
