fd1c6e2179680cb4abeff01e758ba8c846128e71
Prajwal  Kiran Kumar <prajwalkirankumar@wirelessprv-10-194-161-91.near.illinois.edu>
2022-12-01 16:07:41 -0600

    Add user through CLI

--- servicex_app/servicex/cli/add_user.py
+++ servicex_app/servicex/cli/add_user.py
@@ -5,18 +5,13 @@ from servicex.models import UserModel
 from servicex.web.utils import load_app_client
 
 
-def add_user(code):
-    ac = AuthClient(authorizer=AccessTokenAuthorizer(code))
-    id_token = ac.oauth2_userinfo()
-
-    tokens = tokens.by_resource_server,
-
+def add_user(sub, email, name, organization, refresh_token):
     new_user = UserModel(
-        sub=id_token.get('sub'),
-        email=id_token.get('email', ''),
-        name=id_token.get('name', ''),
-        institution=id_token.get('organization', ''),
-        refresh_token=create_refresh_token(identity=id_token.get('sub')))
+        sub=sub,
+        email=email,
+        name=name,
+        institution=organization,
+        refresh_token=refresh_token)
 
     if new_user.email == current_app.config.get('JWT_ADMIN'):
         new_user.admin = True
