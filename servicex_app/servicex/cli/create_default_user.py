25b1ad05599d2ec9595fc1b405d728b90f51dc0b
Prajwal  Kiran Kumar <prajwalkirankumar@Prajwals-MacBook-Pro.local>
2022-12-21 13:39:02 +0530

    Add support for storing user secrets

--- servicex_app/servicex/cli/create_default_user.py
+++ servicex_app/servicex/cli/create_default_user.py
@@ -1,22 +1,25 @@
 from kubernetes import client, config
 import base64
-
+import json
+import os
 config.load_kube_config()
 v1 = client.CoreV1Api()
-secret = v1.read_namespaced_secret("users", "servicex") # get the secret
+secret = v1.read_namespaced_secret("users", "default")
+users_encoded = secret.data['users.json']
+users_decoded = base64.b64decode(users_encoded)
+users = json.loads(users_decoded)
 
-data = secret.data # extract .data from the secret
-print(data)
-# name = secret.metadata.name
-# password = secret.data['password'] # extract .data.password from the secret
-#
-# decoded = base64.b64decode(password) # decode (base64) value from pasw
-#
-# print ("Secret: " + name)
-# print("-------------")
-# print("Here you have the data: ")
-# print(data) 
-# print("COUNTED DATA: " + str(len(data))) # <- ANSWER FOR THE DATA COUNTER
-# print("-------------")
-# print("Here you have the decoded password: ")
-# print(decoded)
+for user in users:
+    command = "poetry run flask --app servicex/app.py create " \
+              "--sub {} " \
+              "--email {} " \
+              "--name {} " \
+              "--institution {} " \
+              "--refresh_token {}".format(user["sub"],
+                                          user["email"],
+                                          user["name"],
+                                          user["institution"],
+                                          user["refresh_token"])
+    os.system(command)
+
+
