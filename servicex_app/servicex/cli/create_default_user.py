34dac99408de8538d8422404b65479948b7589c8
Prajwal  Kiran Kumar <prajwalkirankumar@Prajwals-MacBook-Pro.local>
2022-12-22 17:41:44 +0530

    Add support to add users from k8s secrets

--- servicex_app/servicex/cli/create_default_user.py
+++ servicex_app/servicex/cli/create_default_user.py
@@ -2,7 +2,7 @@ from kubernetes import client, config
 import base64
 import json
 import os
-config.load_kube_config()
+config.load_incluster_config()
 v1 = client.CoreV1Api()
 secret = v1.read_namespaced_secret("users", "default")
 users_encoded = secret.data['users.json']
@@ -10,16 +10,16 @@ users_decoded = base64.b64decode(users_encoded)
 users = json.loads(users_decoded)
 
 for user in users:
-    command = "poetry run flask --app servicex/app.py create " \
-              "--sub {} " \
-              "--email {} " \
-              "--name {} " \
-              "--institution {} " \
-              "--refresh_token {}".format(user["sub"],
-                                          user["email"],
-                                          user["name"],
-                                          user["institution"],
-                                          user["refresh_token"])
+    command = "poetry run flask --app servicex/app.py user create " \
+              "{} " \
+              "{} " \
+              "{} " \
+              "{} " \
+              "{}".format(user["sub"],
+                          user["email"],
+                          user["name"],
+                          user["institution"],
+                          user["refresh_token"])
     os.system(command)
 
 
