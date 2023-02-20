f5dde119ce4abadf76fdd920816ad06f4f388207
Shriram <shriramathreya786@gmail.com>
2023-02-20 12:19:15 -0600

    jwt generation and decoding

---
+++ servicex_app/create_sample_jwt_coffee_casa.py
@@ -0,0 +1,29 @@
+import jwt
+from cryptography.hazmat.primitives import serialization
+
+private_key = open('jwt_rsa', 'r').read()
+
+key_val = serialization.load_ssh_private_key(private_key.encode(), password=b'foo')
+
+payload_data = {
+    "sub": "123456789",
+    "name": "Shriram Athreya",
+    "nickname": "Shriram"
+}
+
+new_token = jwt.encode(
+    payload=payload_data,
+    key=key_val,
+    algorithm='RS256'
+)
+
+print(new_token)
+
+jwt.get_unverified_header(new_token)
+
+public_key = open('jwt_rsa.pub', 'r').read()
+key_decode = serialization.load_ssh_public_key(public_key.encode())
+
+decoded_val = jwt.decode(jwt=new_token, key=key_decode, algorithms=['RS256', ])
+
+print(decoded_val)
