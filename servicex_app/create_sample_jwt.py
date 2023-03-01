e780d892487b217f4428480981e515183d1ac9f0
Shriram <shriramathreya786@gmail.com>
2023-03-01 16:45:56 -0600

    updated code for exp token

--- servicex_app/create_sample_jwt.py
+++ servicex_app/create_sample_jwt.py
@@ -7,9 +7,10 @@ key_val = serialization.load_ssh_private_key(private_key.encode(), password=b'fo
 
 payload = {
   "sub": "bbockelm",
-  "exp": 1509991790,
+  "exp": 1685677084,
   "iat": 1509988190,
   "jti": "515c0084-cb1b-4a63-bce9-b9d6ace17ccb",
+  "iss": "https://scitokens.org/cms",
   "scope": "read:/store write:/store/user/bbockelm",
   "name": "Jane Doe",
   "email": "prajwal@example.com",
