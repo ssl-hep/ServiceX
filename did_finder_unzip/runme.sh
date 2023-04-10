af8b4b1fdc37788927c3a38c277ba1587eb42077
Prajwal  Kiran Kumar <prajwalkirankumar@wirelessprv-10-194-243-226.near.illinois.edu>
2023-04-10 17:33:28 -0500

    add runme

---
+++ did_finder_unzip/runme.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+/usr/src/app/proxy-exporter.sh &
+
+while true; do 
+    date
+    ls ${X509_USER_PROXY}
+    RESULT=$?
+    if [ $RESULT -eq 0 ]; then
+        break
+    fi
+    echo "INFO $INSTANCE_NAME did-finder none Waiting for the proxy."
+    sleep 5
+done
+
+export PYTHONPATH=.
+
+# Assume $REPORT_LOGICAL_FILES is set to --report-logical-files to activate
+echo "----------->$PYTHONPATH"
+ls -lht $PYTHONPATH
+python3 scripts/did_finder.py --rabbit-uri $RMQ_URI $REPORT_LOGICAL_FILES
+
