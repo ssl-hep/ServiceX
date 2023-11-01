4a2871a4f98173efdbc1f29d34a67d694f13c7d2
Ilija Vukotic <ivukotic@uchicago.edu>
2023-11-01 15:03:32 -0500

    f1

--- lite/scripts/sXlite.py
+++ lite/scripts/sXlite.py
@@ -1,12 +1,25 @@
-
 import kubernetes
-from kubernetes import client
+from kubernetes import client, watch
+
+
+class sXorigin:
+    def __init__(self) -> None:
+        kubernetes.config.load_kube_config()
+
+    def watch(self):
+        v1 = client.CoreV1Api()
+        count = 10
+        w = watch.Watch()
+        for event in w.stream(v1.list_namespace, _request_timeout=60):
+            print(f"Event: {event['type']} {event['object'].metadata.name}")
+            count -= 1
+            if not count:
+                w.stop()
 
 
 class sXlite:
-    def __init__(self):
-        kubernetes.config.load_kube_config()
-
+    def __init__(self) -> None:
+        pass
         # client.CoreV1Api().create_namespaced_secret(
         #     namespace=pod_namespace, body=secret)
         # print("Created Secret %s" % secret_name)
@@ -21,4 +34,6 @@ class sXlite:
 
 
 if __name__ == '__main__':
+    sXorigin()
     sXlite()
+    sXorigin.watch()
