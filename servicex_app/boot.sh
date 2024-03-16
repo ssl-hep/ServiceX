c932c7a66b1a8d229d72b25eb73d7b7308ed2daa
Ilija Vukotic <ivukotic@uchicago.edu>
2024-03-16 08:40:53 -0500

    revert number of threads to 1. it leads to double counting.

--- servicex_app/boot.sh
+++ servicex_app/boot.sh
@@ -7,5 +7,5 @@ else
   FLASK_APP=servicex/app.py flask db upgrade;
 fi
 [ -d "/default_users" ] && python3 servicex/cli/create_default_users.py
-exec gunicorn -b :5000 --workers=5 --threads=4 --timeout 120 --log-level=warning --access-logfile /tmp/gunicorn.log --error-logfile - "servicex:create_app()"
+exec gunicorn -b :5000 --workers=5 --threads=1 --timeout 120 --log-level=warning --access-logfile /tmp/gunicorn.log --error-logfile - "servicex:create_app()"
 # to log requests to stdout  --access-logfile -
