c5c9f9ff14e51049913c2aa7d5368cb68cfb39ed
Ben Galewsky <ben@peartreestudio.net>
2024-07-03 09:45:32 -0500

    Pausing work on transformer manager to work on Celery DID Finders

--- did_finder_cernopendata/src/servicex_did_finder_cernopendata/did_finder.py
+++ did_finder_cernopendata/src/servicex_did_finder_cernopendata/did_finder.py
@@ -1,19 +1,19 @@
+import argparse
 import os
 import logging
 from subprocess import PIPE, Popen, STDOUT
-from typing import Any, AsyncGenerator, Dict
-
-from servicex_did_finder_lib import start_did_finder
+from typing import Any, AsyncGenerator, Dict, Generator
+from servicex_did_finder_lib import DIDFinderApp
 
 __log = logging.getLogger(__name__)
 
 cache_prefix = os.environ.get('CACHE_PREFIX', '')
 
 
-async def find_files(did_name: str,
+def find_files(did_name: str,
                      info: Dict[str, Any],
-                     command: str = 'cernopendata-client'
-                     ) -> AsyncGenerator[Dict[str, Any], None]:
+                     did_finder_args: dict = None
+               ) -> Generator[Dict[str, Any], None, None]:
     '''For each incoming did name, generate a list of files that ServiceX can
     process
 
@@ -25,23 +25,20 @@ async def find_files(did_name: str,
 
     Args:
         did_name (str): Dataset name
-        into (Dict[str, Any]): Information bag, mainly has the `request-id` which is
+        info (Dict[str, Any]): Information bag, mainly has the `request-id` which is
                                used to track error mesages accross logs.
         command (str): Command to execute to get the did information. Used only for testing.
 
     Returns:
         AsyncGenerator[Dict[str, any], None]: yield each file
     '''
-    __log.info('DID Lookup request received.', extra={
-               'requestId': info['request-id'], 'dataset': did_name})
+
     if not did_name.isnumeric():
         raise Exception('CERNOpenData can only work with dataset numbers as names (e.g. 1507)')
 
-    cmd = f'{command} get-file-locations --recid {did_name}'.split(' ')
+    cmd = f'cernopendata-client get-file-locations --recid {did_name}'.split(" ")
 
-    with Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1,
-               universal_newlines=1) as p:  # type: ignore
-
+    with Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1,universal_newlines=True) as p:
         assert p.stdout is not None
         all_lines = []
         non_root_uri = False
@@ -49,7 +46,7 @@ async def find_files(did_name: str,
             assert isinstance(line, str)
             all_lines.append(line)
             uri = line.strip()
-            if uri.startswith('http://') and cache_prefix == '':
+            if uri.startswith('XXXXXXXhttp://') and cache_prefix == '':
                 non_root_uri = True
             else:
                 yield {
@@ -72,13 +69,23 @@ async def find_files(did_name: str,
 
 
 def run_open_data():
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--foo', dest='foo', action='store',
+                        default='',
+                        help='Prefix to add to Xrootd URLs')
+
+    DIDFinderApp.add_did_finder_cnd_arguments(parser)
+
     __log.info('Starting CERNOpenData DID finder')
-    try:
-        __log.info('Starting CERNOpenData DID finder',
-                   extra={'requestId': 'forkingfork'})
-        start_did_finder('cernopendata', find_files)
-    finally:
-        __log.info('Done running CERNOpenData DID finder')
+    app = DIDFinderApp('cernopendata', parsed_args=parser.parse_args())
+
+    @app.did_lookup_task(name="did_finder_cern_opendata.lookup_dataset")
+    def lookup_dataset(self, did: str, dataset_id: int, endpoint: str) -> None:
+        self.do_lookup(did=did, dataset_id=dataset_id,
+                       endpoint=endpoint, user_did_finder=find_files)
+
+    app.start()
 
 
 if __name__ == "__main__":
