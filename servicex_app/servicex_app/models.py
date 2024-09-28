1ad6e29b00f0a529be37f328cad29365473218c6
Peter Onyisi <ponyisi@utexas.edu>
2024-09-28 17:12:18 -0500

    pre-commit

--- servicex_app/servicex_app/models.py
+++ servicex_app/servicex_app/models.py
@@ -279,12 +279,14 @@ class TransformRequest(db.Model):
 
     @classmethod
     def file_transformed_successfully(cls, key: Union[str, int]) -> None:
+        db.session.commit()
         req = cls.query.filter_by(request_id=key).one()
         req.files_completed = cls.files_completed + 1
         db.session.commit()
 
     @classmethod
     def file_transformed_unsuccessfully(cls, key: Union[str, int]) -> None:
+        db.session.commit()
         req = cls.query.filter_by(request_id=key).one()
         req.files_failed = cls.files_failed + 1
         db.session.commit()
