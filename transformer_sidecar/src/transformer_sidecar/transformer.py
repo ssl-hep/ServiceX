35835b77764f325b6229ed7b345a3a3222e35849
Ben Galewsky <ben@peartreestudio.net>
2024-10-11 09:57:18 -0600

    transform_file task uses ack_late=True We found that when the autoscaler decides to scale down the number of transformers that the science image dies immediatly which  causes the "sync" call to fail. When this happens we need to put the transform request back onto rabbit queue and die gracefully.
    Add the acks_late propoerty to the transform_file request and now the failure
    to receive the sync command from the science container is a fatal error.

--- transformer_sidecar/src/transformer_sidecar/transformer.py
+++ transformer_sidecar/src/transformer_sidecar/transformer.py
@@ -29,6 +29,7 @@
 import json
 import os
 import shutil
+import sys
 import timeit
 from argparse import Namespace
 from hashlib import sha1, sha256
@@ -42,7 +43,8 @@ import psutil as psutil
 import time
 from celery import Celery, shared_task
 
-from transformer_sidecar.science_container_command import ScienceContainerCommand
+from transformer_sidecar.science_container_command import ScienceContainerCommand, \
+    ScienceContainerException
 from transformer_sidecar.transformer_logging import initialize_logging
 from transformer_sidecar.transformer_stats import TransformerStats
 from transformer_sidecar.transformer_stats.aod_stats import AODStats  # NOQA: 401
@@ -82,7 +84,7 @@ start_time = timeit.default_timer()
 logger = initialize_logging()
 
 
-@shared_task
+@shared_task(acks_late=True)
 def transform_file(
         request_id,
         file_id,
@@ -276,6 +278,9 @@ def transform_file(
             },
         )
 
+    except ScienceContainerException:
+        logger.exception("Science container not responding. Shutting down this transformer.")
+        sys.exit(0)
     except Exception as error:
         logger.exception(f"Received exception doing transform: {error}")
         rec = FileCompleteRecord(
