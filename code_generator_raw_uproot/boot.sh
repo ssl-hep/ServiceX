7d2dee63a8bd7e3aed41c0075a0d2dda342c6075
Peter Onyisi <ponyisi@utexas.edu>
2024-11-15 16:41:18 -0600

    IPv6-ize rest of services

--- code_generator_raw_uproot/boot.sh
+++ code_generator_raw_uproot/boot.sh
@@ -4,7 +4,7 @@
 action=${1:-web_service}
 if [ "$action" = "web_service" ] ; then
     mkdir instance
-    exec gunicorn -b :5000 --workers=2 --threads=1 --access-logfile - --error-logfile - "servicex.raw_uproot_code_generator:create_app()"
+    exec gunicorn -b [::]:5000 --workers=2 --threads=1 --access-logfile - --error-logfile - "servicex.raw_uproot_code_generator:create_app()"
 else
     echo "Unknown action '$action'"
 fi
