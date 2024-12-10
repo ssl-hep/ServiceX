4a4ca9abdaf4abf2ace194c22ed24b85874ded51
Ben Galewsky <ben@peartreestudio.net>
2024-12-10 16:17:59 -0600

    Remove unused methods on TransformRequest

--- servicex_app/servicex_app/models.py
+++ servicex_app/servicex_app/models.py
@@ -279,24 +279,6 @@ class TransformRequest(db.Model):
         except NoResultFound:
             return []
 
-    @classmethod
-    def file_transformed_successfully(cls, key: Union[str, int]) -> None:
-        req = cls.query.filter_by(request_id=key).one()
-        req.files_completed = cls.files_completed + 1
-        db.session.commit()
-
-    @classmethod
-    def file_transformed_unsuccessfully(cls, key: Union[str, int]) -> None:
-        req = cls.query.filter_by(request_id=key).one()
-        req.files_failed = cls.files_failed + 1
-        db.session.commit()
-
-    @classmethod
-    def add_a_file(cls, key) -> None:
-        req = cls.query.filter_by(request_id=key).one()
-        req.files = TransformRequest.files + 1
-        db.session.commit()
-
     @property
     def age(self) -> timedelta:
         return datetime.utcnow() - self.submit_time
