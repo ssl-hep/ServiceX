e1e5e67adcd264cb923d3fb66f3d58ea42f20a28
alexancs <alexcschmidt17@gmail.com>
2025-03-04 13:38:26 -0600

    Deleted files and changes

--- local_test_harness/temp1/generated_transformer.py
+++
@@ -1,9 +0,0 @@
-
-import yaml
-import os
-import json
-def make_yaml():
-    query = [{'treename': {'nominal': 'modified'}, 'filter_name': ['lbn']}]
-    with open(os.path.join(os.environ.get("CONFIG_LOC"), "reco.yaml"), 'w') as reco:
-        yaml.dump(query, reco, default_flow_style=False)
-    
