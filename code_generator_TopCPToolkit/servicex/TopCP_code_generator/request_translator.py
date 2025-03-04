4c374aa2442c10e7a4238b4b30d8c2539cb232ac
alexancs <alexcschmidt17@gmail.com>
2025-03-04 13:26:06 -0600

    updated TopCPToolkit codegen to use runTop_el options

--- code_generator_TopCPToolkit/servicex/TopCP_code_generator/request_translator.py
+++ code_generator_TopCPToolkit/servicex/TopCP_code_generator/request_translator.py
@@ -27,6 +27,8 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import os
 import shutil
+from . import query_translate
+import json
 
 from servicex_codegen.code_generator import CodeGenerator, GeneratedFileResult, \
     GenerateCodeException
@@ -55,7 +57,9 @@ class TopCPTranslator(CodeGenerator):
                                            "/home/servicex/transformer_capabilities.json")
         shutil.copyfile(capabilities_path, os.path.join(query_file_path,
                                                         "transformer_capabilities.json"))
-        with open(os.path.join(query_file_path, "reco.yaml"),"w") as reco:
-                reco.write(query)
+
+        query_translate.generate_files_from_query(query, query_file_path)
 
         return GeneratedFileResult(_hash, query_file_path)
+
+    
