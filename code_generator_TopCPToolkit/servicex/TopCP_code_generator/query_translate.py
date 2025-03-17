117b1c2e83fcdf0f1e88edb5b344c3ab0b3c4988
KyungEon Choi <kyungeon@gmail.com>
2025-03-17 11:10:28 -0500

    Drop obsolete fields

--- code_generator_TopCPToolkit/servicex/TopCP_code_generator/query_translate.py
+++ code_generator_TopCPToolkit/servicex/TopCP_code_generator/query_translate.py
@@ -2,63 +2,64 @@ import json
 import os
 
 options = {
-    "reco":{
+    "reco": {
         "properType": str,
-        "properTypeString":"string",
+        "properTypeString": "string",
         "fileName": "reco.yaml",
     },
-    "parton":{
+    "parton": {
         "properType": str,
-        "properTypeString":"string",
+        "properTypeString": "string",
         "fileName": "parton.yaml",
     },
-     "particle":{
+    "particle": {
         "properType": str,
-        "properTypeString":"string",
+        "properTypeString": "string",
         "fileName": "particle.yaml",
     },
-    "max_events":{
+    "max_events": {
         "properType": int,
-        "properTypeString":"integer",
-        "default":["-e", "-1"],
+        "properTypeString": "integer",
+        "default": ["-e", "-1"],
         "option": "-e",
-        "minimum":-1
+        "minimum": -1
     },
-    "no_reco":{
+    # "no_reco":{
+    #     "properType": bool,
+    #     "properTypeString":"boolean",
+    #     "ifTrue": ["--no-reco"],
+    #     "ifFalse": None,
+    # },
+    # "run_parton":{
+    #     "properType": bool,
+    #     "properTypeString":"boolean",
+    #     "ifTrue": ["--parton"],
+    #     "ifFalse": None,
+    # },
+    # "run_particle":{
+    #     "properType": bool,
+    #     "properTypeString":"boolean",
+    #     "ifTrue": ["--particle"],
+    #     "ifFalse": None,
+    # },
+    "no_systematics": {
         "properType": bool,
-        "properTypeString":"boolean",
-        "ifTrue": ["--no-reco"],
-        "ifFalse": None,
-    },
-    "run_parton":{
-        "properType": bool,
-        "properTypeString":"boolean",
-        "ifTrue": ["--parton"],
-        "ifFalse": None,
-    },
-    "run_particle":{
-        "properType": bool,
-        "properTypeString":"boolean",
-        "ifTrue": ["--particle"],
-        "ifFalse": None,
-    },
-    "no_systematics":{
-        "properType": bool,
-        "properTypeString":"boolean",
+        "properTypeString": "boolean",
         "ifTrue": ["--no-systematics"],
         "ifFalse": None,
     },
-    "no_filter":{
+    "no_filter": {
         "properType": bool,
-        "properTypeString":"boolean",
+        "properTypeString": "boolean",
         "ifTrue": ["--no-filter"],
         "ifFalse": None,
     }
-    }
+}
+
 
 def generate_files_from_query(query, query_file_path):
     jquery = json.loads(query)
-    
+
     runTopCommand = ["runTop_el.py", "-i", "input.txt", "-o", "output", "-t", "customConfig"]
 
     # ensure all keys are specified
@@ -67,21 +68,21 @@ def generate_files_from_query(query, query_file_path):
             raise ValueError(key+" must be specified. May be type None or ", options[key]["properTypeString"])
 
     for key in jquery:
-        # ensure only aviable options are allowed  
+        # ensure only aviable options are allowed
         if key not in options:
             raise KeyError(key + " is not implemented. Available keys: " + str(options.keys()))
-        
+
         # ensure None entries are ignored
         if jquery[key] is None:
             continue
 
-        # type check key            
+        # type check key
         if not isinstance(jquery[key], options[key]["properType"]):
-            raise TypeError(key+" must be of type "+ options[key]["properTypeString"])
+            raise TypeError(key+" must be of type " + options[key]["properTypeString"])
 
         # check for reco.yaml, parton.yaml and particle.yaml files
-        if isinstance(jquery[key], str): 
-            with open(os.path.join(query_file_path, options[key]["fileName"]),"w") as file:
+        if isinstance(jquery[key], str):
+            with open(os.path.join(query_file_path, options[key]["fileName"]), "w") as file:
                 file.write(jquery[key])
 
         # check for toggle option
@@ -93,19 +94,19 @@ def generate_files_from_query(query, query_file_path):
 
             if options[key][optStr] is not None:
                 runTopCommand.extend(options[key][optStr])
-        
+
         # check max events and skip events
         elif isinstance(jquery[key], int):
-            if jquery[key]< options[key]["minimum"]:
+            if jquery[key] < options[key]["minimum"]:
                 raise ValueError(key + " cannot be less than " + str(options[key]["minimum"]))
             else:
                 runTopCommand.extend([options[key]["option"], str(jquery[key])])
-    
+
     # make generated_transformer.py
-    generated_code= f'''
+    generated_code = f'''
 import subprocess
 def runTop_el():
     subprocess.run({runTopCommand})
 '''
     with open(os.path.join(query_file_path, 'generated_transformer.py'), 'w') as python_file:
-            python_file.write(generated_code)    
+        python_file.write(generated_code)
