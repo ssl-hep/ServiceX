edc849a64005fd6ecaee89f49897e2b3b4bd6bc1
kondratyevd <kondratyev.d.95@gmail.com>
2025-06-23 16:51:36 -0400

    add quotes in codegen

--- code_generator_raw_uproot/servicex/raw_uproot_code_generator/request_translator.py
+++ code_generator_raw_uproot/servicex/raw_uproot_code_generator/request_translator.py
@@ -75,7 +75,7 @@ class RawUprootTranslator(CodeGenerator):
 
         generated_code = f'''
 import os
-os.environ['COMPRESSION_ALGORITHM'] = {compression_algorithm}
+os.environ['COMPRESSION_ALGORITHM'] = '{compression_algorithm}'
 os.environ['COMPRESSION_LEVEL'] = {compression_level}
 
 def run_query(file_path):
