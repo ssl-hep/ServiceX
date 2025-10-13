5d333535dea6ae5bfbed54cde91fbe6e265ad275
Peter Onyisi <ponyisi@utexas.edu>
2025-10-13 13:51:05 -0500

    Fix something that must have been a mistake

--- servicex_app/servicex_app_test/web/test_auth_callback.py
+++ servicex_app/servicex_app_test/web/test_auth_callback.py
@@ -39,7 +39,8 @@ class TestAuthCallback(WebTestBase):
         response: Response = client.get(
             url_for("auth_callback"), query_string={"code": "oauth-code"}
         )
-        oauth_client.authorize_access_token.assert_called_once()
+        mock_oauth.authorize_redirect.assert_not_called()
+        mock_user_model.find_by_email.assert_called_once()
         id_token = self._id_token()
         assert mock_session.get("is_authenticated")
         assert mock_session.get("name") == id_token["name"]
