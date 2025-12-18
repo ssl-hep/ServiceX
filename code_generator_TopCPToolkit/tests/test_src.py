dc243f3628a61dd69f07a05d51d685b9d84d4b09
Matt Shirley <mattutshirley@gmail.com>
2025-12-18 11:14:51 -0600

    remove unneeded test

--- code_generator_TopCPToolkit/tests/test_src.py
+++ code_generator_TopCPToolkit/tests/test_src.py
@@ -88,52 +88,6 @@ def test_generate_code():
             translator.generate_code(query, tmpdirname)
 
 
-def test_generate_code_with_custom_docker_image():
-    os.environ["TEMPLATE_PATH"] = "servicex/templates/transform_single_file.py"
-    os.environ["CAPABILITIES_PATH"] = "transformer_capabilities.json"
-    os.environ["ALLOWED_DOCKER_REGISTRIES"] = (
-        '{"docker.io": {"allowedImagePrefixes": ["sslhep/custom_image:"]}}'
-    )
-
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        translator = TopCPTranslator()
-        query = (
-            '{"reco": "CommonServices:\\n  systematicsHistogram: \'listOfSystematics\'\\n\\n'
-            "PileupReweighting: {}\\n\\nEventCleaning:\\n    runEventCleaning: False\\n"
-            "    runGRL: False\\n\\nElectrons:\\n  - containerName: 'AnaElectrons'\\n"
-            "    crackVeto: True\\n    IFFClassification: {}\\n    WorkingPoint:\\n"
-            "      - selectionName: 'loose'\\n        identificationWP: 'TightLH'\\n"
-            "        isolationWP: 'NonIso'\\n        noEffSF: True\\n"
-            "      - selectionName: 'tight'\\n        identificationWP: 'TightLH'\\n"
-            "        isolationWP: 'Tight_VarRad'\\n        noEffSF: True\\n"
-            "    PtEtaSelection:\\n        minPt: 25000.0\\n        maxEta: 2.47\\n"
-            "        useClusterEta: True\\n\\n"
-            "# After configuring each container, many variables will be saved automatically.\\n"
-            "Output:\\n  treeName: 'reco'\\n  vars: []\\n  metVars: []\\n  containers:\\n"
-            "      # Format should follow: '<suffix>:<output container>'\\n"
-            "      el_: 'AnaElectrons'\\n      '': 'EventInfo'\\n  commands:\\n"
-            "    # Turn output branches on and off with 'enable' and 'disable'\\n\\n"
-            'AddConfigBlocks: []\\n", "parton": null, "particle": null, "max_events": 100, '
-            '"no_systematics": true, "no_filter": false, '
-            '"image": "sslhep/custom_image:test"}'
-        )
-
-        expected_hash = "cd803069c31c5fa1789eb98f3ea53011"
-        result = translator.generate_code(query, tmpdirname)
-
-        # is the generated code at least syntactically valid Python?
-        try:
-            exec(
-                open(os.path.join(result.output_dir, "generated_transformer.py")).read()
-            )
-        except SyntaxError:
-            pytest.fail("Generated Python is not valid code")
-
-        assert result.hash == expected_hash
-        assert result.image == "sslhep/custom_image:test"
-        assert result.output_dir == os.path.join(tmpdirname, expected_hash)
-
-
 def test_generate_code_fails_with_unknown_selection_key():
     os.environ["TEMPLATE_PATH"] = "servicex/templates/transform_single_file.py"
     os.environ["CAPABILITIES_PATH"] = "transformer_capabilities.json"
