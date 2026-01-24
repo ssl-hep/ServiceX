59af9cd5cbc65bc82e85465a527caa6e800f35aa
Peter Onyisi <ponyisi@utexas.edu>
2026-01-23 21:57:53 -0600

    Flake8 fix

--- servicex_app/servicex_app_test/resources/internal/test_dataset_lifecycle_ops.py
+++ servicex_app/servicex_app_test/resources/internal/test_dataset_lifecycle_ops.py
@@ -25,7 +25,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-from datetime import datetime, timezone
+from datetime import datetime
 from unittest.mock import patch
 
 from pytest import fixture
