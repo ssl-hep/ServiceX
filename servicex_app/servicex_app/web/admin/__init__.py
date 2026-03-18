15f3ac7a51eb1cc71998da11490d1914328ea0a7
Matt Shirley <mattutshirley@gmail.com>
2026-03-18 14:46:41 -0500

    resolve flake8

--- servicex_app/servicex_app/web/admin/__init__.py
+++ servicex_app/servicex_app/web/admin/__init__.py
@@ -1,5 +1,4 @@
 from flask import current_app, redirect, request, session, url_for
-from flask_admin import expose
 from flask_jwt_extended import verify_jwt_in_request
 from flask_jwt_extended.exceptions import NoAuthorizationError
 
