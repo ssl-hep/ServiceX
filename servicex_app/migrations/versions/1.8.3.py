63907ffdf2ffb8bd8c9be0f77c29895709341738
Matt Shirley <mattutshirley@gmail.com>
2026-03-25 14:40:45 -0500

    fix migration version and remove duplicate admin check logic

--- servicex_app/migrations/versions/1.8.3.py
+++ servicex_app/migrations/versions/1.8.3.py
@@ -1,15 +1,14 @@
 """
 add index to UserModel.sub
 
-Revision ID: v1_7_4
+Revision ID: v1_8_3
 Revises: v1_7_3
 Create Date: 2026-03-04 20:52:15.921295
 """
 
 from alembic import op
 
-# revision identifiers, used by Alembic.
-revision = "v1_7_4"
+revision = "v1_8_3"
 down_revision = "v1_7_3"
 branch_labels = None
 depends_on = None
