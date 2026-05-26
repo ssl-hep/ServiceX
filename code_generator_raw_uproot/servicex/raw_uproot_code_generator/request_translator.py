643433cc10a734cefdbde02543eca12877ab2219
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2026-05-26 16:17:35 -0400

    Allow compression level 0

--- code_generator_raw_uproot/servicex/raw_uproot_code_generator/request_translator.py
+++ code_generator_raw_uproot/servicex/raw_uproot_code_generator/request_translator.py
@@ -35,7 +35,7 @@ from servicex_codegen.code_generator import (
 )
 
 ALLOWED_COMPRESSION_ALGORITHMS = {"ZLIB", "LZMA", "LZ4", "ZSTD"}
-ALLOWED_COMPRESSION_LEVELS = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+ALLOWED_COMPRESSION_LEVELS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 
 class RawUprootTranslator(CodeGenerator):
