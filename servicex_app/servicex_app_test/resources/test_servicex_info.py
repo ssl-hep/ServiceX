35fab8519a43110b7e436178bcf2ed0e4069f510
Matt Shirley <mattutshirley@gmail.com>
2026-06-22 15:09:56 -0500

    add capabilities entry for this

--- servicex_app/servicex_app_test/resources/test_servicex_info.py
+++ servicex_app/servicex_app_test/resources/test_servicex_info.py
@@ -44,5 +44,6 @@ class TestServicexInfo(ResourceTestBase):
             "capabilities": [
                 "poll_local_transformation_results",
                 "long_sample_titles_10240",
+                "post_cancel_transform"
             ],
         }  # noqa: E501
