c3c5b5e7749af04822f6307d83359974d8339b7a
Ben Galewsky <ben@peartreestudio.net>
2026-08-07 15:27:53 -0500

    If you aren't logged in, then the home page should be the login

--- servicex_app/servicex_app_test/web/test_home.py
+++ servicex_app/servicex_app_test/web/test_home.py
@@ -1,4 +1,4 @@
-from flask import Response, template_rendered
+from flask import Response
 from pytest import fixture
 
 from .web_test_base import WebTestBase
@@ -10,30 +10,17 @@ class TestHome(WebTestBase):
     def auth_client(self):
         return self._test_client(extra_config={"ENABLE_AUTH": True})
 
-    @fixture
-    def auth_captured_templates(self, auth_client):
-        recorded = []
-
-        def record(sender, template, context, **extra):
-            recorded.append((template, context))
-
-        template_rendered.connect(record, auth_client.application)
-        try:
-            yield recorded
-        finally:
-            template_rendered.disconnect(record, auth_client.application)
-
     def test_home_without_auth(self, client):
         """With auth disabled, everyone lands on the global dashboard."""
         response: Response = client.get("/")
         assert response.status_code == 302
         assert response.headers["Location"].endswith("/global-dashboard")
 
-    def test_home_signed_out(self, auth_client, auth_captured_templates):
+    def test_home_signed_out(self, auth_client):
+        """With auth enabled, signed-out visitors are sent to sign in."""
         response: Response = auth_client.get("/")
-        assert response.status_code == 200
-        template, _ = auth_captured_templates[0]
-        assert template.name == "home.html"
+        assert response.status_code == 302
+        assert response.headers["Location"].endswith("/sign-in")
 
     def test_home_signed_in(self, auth_client, user):
         with auth_client.session_transaction() as sess:
